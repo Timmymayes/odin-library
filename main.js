acfc0658e8aed64ade1a89bd2adfa70131988f5e
Tyler Mayes <timmymayes@gmail.com>
2022-11-27 10:32:16 -0800

    Updated page struger to be table like

--- main.js
+++ main.js
@@ -28,3 +28,37 @@ function addBookToLibrary(book) {
   // do stuff here
   myLibrary.push(book);
 }
+
+
+// some test books
+let book01 = new Book(
+  author = "Donella Meadows";
+  title = "Systems Thinking";
+  pages = 500 ;
+  isRead = false;
+)
+
+let book02 = new Book(
+  author = "David Goggins";
+  title = "Can't Hurt Me";
+  pages = 800;
+  isRead = true;
+)
+
+let book03 = new Book(
+  author = "Seth Godin";
+  title = "Purple Cow";
+  pages = 450;
+  isRead = false;
+)
+let book04 = new Book(
+  author = "John Carmack";
+  title = "Masters Of Doom";
+  pages = 350;
+  isRead = false;
+)
+
+addBookToLibrary(book01);
+addBookToLibrary(book02);
+addBookToLibrary(book03);
+addBookToLibrary(book04);
