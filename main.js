3297788138e69725c3c4feae6c7a85624e392caf
Tyler Mayes <timmymayes@gmail.com>
2022-12-02 11:25:04 -0800

    Cleaned up main.js and refactored addBookDataToRow
     Helper Functions Added
     - createStatusButton
     - isButton
     - determineClassToAdd
     - createButtonText
     - checkIfNeedsCenteredAndCenter
     - checkIfRowNeedsAltStyleAndAddClass

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let myLibrary = [];
+let bookList = document.getElementById("book-list");
+let rowCounter = 0;
+let newRow;
+let mainButton = document.getElementById("main-btn");
+let form = document.getElementById("form");
 
 function Book(author, title, pages, readStatus) {
   // Constructor
@@ -17,6 +22,12 @@ function Book(author, title, pages, readStatus) {
   this.isRead = readStatus;
 }
 
+function toggleStatus(e) {
+  //Toggle button status by calling prototype function
+  let curBook = myLibrary[e.path[2].id];
+  e.target.childNodes[0].nodeValue = curBook.toggleReadStatus();
+}
+
 Book.prototype.toggleReadStatus = function () {
   if (this.isRead === true) {
     this.isRead = false;
@@ -25,71 +36,131 @@ Book.prototype.toggleReadStatus = function () {
     this.isRead = true;
     return "Complete";
   }
-  // https://www.w3schools.com/howto/howto_css_switch.asp
+  // eventually conver to a switch  https://www.w3schools.com/howto/howto_css_switch.asp
 };
-
-function toggleStatus(e) {
-  let curBook = myLibrary[e.path[2].id];
-  e.target.childNodes[0].nodeValue = curBook.toggleReadStatus();
-  console.log(e.target.childNodes[0].nodeValue);
-}
 
 function addBookToLibrary(book) {
   // add book to array and grab the last element and add book to list
   myLibrary.push(book);
-  console.log();
 }
 
 function addBookDataToRow(row, book) {
   Object.entries(book).forEach((entry) => {
     let td = document.createElement("td");
     const [key, value] = entry;
-    console.log(key, ":", value);
-    let classToAdd = key;
-    if (key === "isRead") {
-      let readStatus;
-      let btn = document.createElement("button");
-      classToAdd = "status";
-      if (value === true) {
-        readStatus = "Complete";
-      } else {
-        readStatus = "Not Read";
-      }
-      btn.classList.add("readStatus");
-      btn.appendChild(document.createTextNode(readStatus));
-      // Commented out for now
-      btn.addEventListener("click", toggleStatus);
-      td.appendChild(btn);
-      td.classList.add("center");
+    td.classList.add(determineClassToAdd(key));
+    if (isButton(key)) {
+      td.appendChild(createStatusButton(value));
     } else {
       td.appendChild(document.createTextNode(value));
     }
-    td.classList.add(classToAdd);
-    if (key === "pages") {
-      td.classList.add("center");
-    }
+    checkIfNeedsCenteredAndCenter(key);
     row.appendChild(td);
   });
+}
+
+function createStatusButton(buttonState) {
+  let readStatus;
+  let btn = document.createElement("button");
+  btn.classList.add("readStatus");
+  btn.appendChild(document.createTextNode(createButtonText(buttonState)));
+  btn.addEventListener("click", toggleStatus);
+  return btn;
+}
+
+function isButton(propertyName) {
+  if (propertyName == "isRead") {
+    return true;
+  }
+  return false;
+}
+
+function determineClassToAdd(propertyName) {
+  if (propertyName === "isRead") {
+    return "status";
+  } else {
+    return propertyName;
+  }
+}
+
+function createButtonText(propertyValue) {
+  if (propertyValue === true) {
+    return "Complete";
+  } else {
+    return "Not Read";
+  }
+}
+
+function checkIfNeedsCenteredAndCenter(tableData, propertyName) {
+  if (propertyName === "page" || propertyName === "isRead") {
+    tableData.classList.add("center");
+  }
 }
 
 function createTableAndAddBook(book) {
   newRow = document.createElement("tr");
   addBookDataToRow(newRow, book);
-  if (rowCounter % 2 != 0) {
-    newRow.classList.add("alt");
-  }
-  newRow.id = rowCounter;
+  checkIfRowNeedsAltStyleAndAddClass(newRow, rowCounter);
+  newRow.id = "Row_" + rowCounter;
   rowCounter++;
   bookList.appendChild(newRow);
   modal.style.display = "none";
   form.reset();
 }
 
-// function addNewBook(e) {
-//   console.log(e);
-//   e.preventDefault();
-//   const data = new FormData(form);
-// }
+function checkIfRowNeedsAltStyleAndAddClass(row, rowNumber) {
+  if (rowNumber % 2 != 0) {
+    row.classList.add("alt");
+  }
+}
+
+//take form data by short circuiting the default action
+//then capture form data and create a book
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let bookData = [];
+  const submittedData = new FormData(form);
+
+  for (const [key, value] of submittedData) {
+    bookData.push(value);
+    bookData;
+  }
+  //if the checkbox for read/not read passes a string
+  //  if true and  it simply leaves the data off if not
+  // so we test length and add the needed boolean.
+  if (bookData.length < 4) {
+    bookData.push(false);
+    bookData;
+  } else {
+    bookData[3] = true;
+  }
+  //
+  let bookToAdd = new Book(
+    (author = bookData[0]),
+    (title = bookData[1]),
+    (pages = Number(bookData[2])),
+    (isRead = bookData[3])
+  );
+  addBookToLibrary(bookToAdd);
+  createTableAndAddBook(bookToAdd);
+});
+
+// Form modal creation
+var modal = document.getElementById("addBookModal");
+var span = document.getElementsByClassName("close")[0];
+mainButton.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 // some test books
 let book01 = new Book(
@@ -119,70 +190,13 @@ let book04 = new Book(
   (isRead = false)
 );
 
+// add test books to library and load them on page
+
 addBookToLibrary(book01);
 addBookToLibrary(book02);
 addBookToLibrary(book03);
 addBookToLibrary(book04);
 
-let bookList = document.getElementById("book-list");
-let rowCounter = 0;
-let newRow;
-let mainButton = document.getElementById("main-btn");
-let form = document.getElementById("form");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let result = [];
-  const data = new FormData(form);
-  /*
-
-A long ass
-
-comment
-oh yeah
-*/
-  for (const [key, value] of data) {
-    result.push(value);
-    console.log(result);
-  }
-  if (result.length < 4) {
-    result.push(false);
-    console.log(result);
-  } else {
-    result[3] = true;
-  }
-  let bookToAdd = new Book(
-    (title = result[1]),
-    (author = result[0]),
-    (pages = Number(result[2])),
-    (isRead = result[3])
-  );
-  console.log(bookToAdd);
-  addBookToLibrary(bookToAdd);
-  createTableAndAddBook(bookToAdd);
-});
-
 myLibrary.forEach((item) => {
-  newRow = document.createElement("tr");
-  addBookDataToRow(newRow, item);
-  if (rowCounter % 2 != 0) {
-    newRow.classList.add("alt");
-  }
-  newRow.id = rowCounter;
-  bookList.appendChild(newRow);
-  rowCounter++;
+  createTableAndAddBook(item);
 });
-
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
-mainButton.onclick = function () {
-  modal.style.display = "block";
-};
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
